9551ea4da5ae249ae97fa18ba5cb52afe1736264
delbury <592804814@qq.com>
2020-06-22 23:38:48 +0800

    项目初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,8 +1,7 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view>
-			<text class="title">{{title}}</text>
+			<navigator url="/pages/space/space">空间</navigator>
 		</view>
 	</view>
 </template>
---
+++ src/pages/space/space.vue
@@ -0,0 +1,35 @@
+<template>
+	<view>
+    <wuc-tab :tabList="tabList" tabClass="space-tab"></wuc-tab>
+	</view>
+</template>
+
+<script>
+  import WucTab from '@/components/wuc-tab/wuc-tab.vue'
+	export default {
+    components: { WucTab },
+		data() {
+			return {
+        tabList: [
+          { name: '客厅' }
+        ]
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+  .space-tab {
+    background-color: $uni-color-primary;
+    
+    /deep/ .wuc-tab-item {
+      height: 36px;
+      line-height: 36px;
+      font-size: 16px;
+      color: #fff;
+    }
+  }
+</style>
